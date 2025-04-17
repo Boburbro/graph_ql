c2ab0b96129b57f137c780d5565ad65def568d7b
Bobur Otaboyev <boburotaboyev0@gmail.com>
2025-04-17 16:46:37 +0500

    send gmail fixed (i think)

--- test-email.js
+++ test-email.js
@@ -2,28 +2,37 @@ const nodemailer = require('nodemailer');
 
 // Create transporter
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
+    host: '142.250.102.108', // Direct IP for smtp.gmail.com
     port: 587,
     secure: false,
     requireTLS: true,
+    name: 'smtp.gmail.com', // Required when using direct IP
     auth: {
         user: 'assoftuz@gmail.com',
         pass: 'goje kwnw xvdm qhug'
     },
     tls: {
         rejectUnauthorized: false,
-        minVersion: "TLSv1.2"
+        minVersion: "TLSv1.2",
+        servername: 'smtp.gmail.com' // SNI for TLS
     },
     debug: true,
     logger: true,
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000
+    connectionTimeout: 30000, // Increased timeout to 30 seconds
+    greetingTimeout: 30000,
+    socketTimeout: 30000
 });
 
 // Test email
 async function testEmail() {
     try {
+        console.log('Starting with configuration:', {
+            host: transporter.options.host,
+            port: transporter.options.port,
+            secure: transporter.options.secure,
+            user: transporter.options.auth.user
+        });
+
         console.log('Verifying SMTP connection...');
         const verify = await transporter.verify();
         console.log('SMTP connection verified:', verify);
@@ -43,7 +52,8 @@ async function testEmail() {
             code: error.code,
             command: error.command,
             message: error.message,
-            stack: error.stack
+            stack: error.stack,
+            errorInfo: error.response
         });
     }
 }
